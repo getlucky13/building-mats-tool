7e15f8a5e8b22ffb5153a71469af2a6b618da5a1
Ron Rizzo <churchmouth13@gmail.com>
2023-12-22 10:53:27 -0500

    Remove BBPWall and make changes to BBPKit
    Removed BBPKit, instead will store values as constants in piece objects
    for memory efficiency. Less method calls and object creation.

--- BBPWall.java
+++ BBPWall.java
@@ -1,13 +1,14 @@
 public class BBPWall {
     private String size, type;
     private int tier, nails, planks, sheetMetal, morterMix, concreteBricks;
-    private BBPKit frame;
+    final int kitNails = 3;
+    final int kitPlanks = 3;
     
     public BBPWall(){
         this.size = "Large";
         this.type = "Full";
         tier = 1;
-        nails = 20;
-
+        nails = 20 + kitNails;
+        planks = 11 + kitPlanks;
     }
 }
