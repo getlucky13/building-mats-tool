f09b366fa7b72739bce602456450048344c1cbff
Ron Rizzo <churchmouth13@gmail.com>
2023-12-22 10:58:32 -0500

    Moved kitNails and kitPlanks to BBPPiece
    Made kitNails and kitPlanks attributes of BBPPiece with constant values.
    Added implementation of BBPPiece to BBPWall and added placeholders for
    getMats and calcMats.

--- BBPPiece.java
+++ BBPPiece.java
@@ -1,6 +1,8 @@
 import java.util.*;
 
 public interface BBPPiece {
+    final int kitNails = 3;
+    final int kitPlanks = 3;
     public void getMats();
     public HashMap<String, Integer> calcMats(int amount);
 }
--- BBPWall.java
+++ BBPWall.java
@@ -1,14 +1,84 @@
-public class BBPWall {
+public class BBPWall implements BBPPiece {
     private String size, type;
     private int tier, nails, planks, sheetMetal, morterMix, concreteBricks;
-    final int kitNails = 3;
-    final int kitPlanks = 3;
-    
+
     public BBPWall(){
         this.size = "Large";
         this.type = "Full";
         tier = 1;
         nails = 20 + kitNails;
         planks = 11 + kitPlanks;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getTier() {
+        return tier;
+    }
+
+    public void setTier(int tier) {
+        this.tier = tier;
+    }
+
+    public int getNails() {
+        return nails;
+    }
+
+    public void setNails(int nails) {
+        this.nails = nails;
+    }
+
+    public int getPlanks() {
+        return planks;
+    }
+
+    public void setPlanks(int planks) {
+        this.planks = planks;
+    }
+
+    public int getSheetMetal() {
+        return sheetMetal;
+    }
+
+    public void setSheetMetal(int sheetMetal) {
+        this.sheetMetal = sheetMetal;
+    }
+
+    public int getMorterMix() {
+        return morterMix;
+    }
+
+    public void setMorterMix(int morterMix) {
+        this.morterMix = morterMix;
+    }
+
+    public int getConcreteBricks() {
+        return concreteBricks;
+    }
+
+    public void setConcreteBricks(int concreteBricks) {
+        this.concreteBricks = concreteBricks;
+    }
+
+    public void getMats() {
+
+    }
+
+    public HashMap calMats(){
+        
     }
 }
