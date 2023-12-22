b0fcab08ad9ccafb656e8b7dd44ff4561ca4a0b0
Ron Rizzo <churchmouth13@gmail.com>
2023-12-22 10:36:12 -0500

    Remove MatMap and update BBBPiece
    Decided to approach materials differently; as such removed MatMap.
    Will use HashMap instead.

--- BBPPiece.java
+++ BBPPiece.java
@@ -1,4 +1,6 @@
+import java.util.*;
+
 public interface BBPPiece {
     public void getMats();
-    public matMap calcMats(int tier, int amount);
+    public HashMap<String, Integer> calcMats(int amount);
 }
